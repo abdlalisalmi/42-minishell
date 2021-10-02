5d367306bc16bd15b259f7d0ece828e22e7b7c08
Amine Tahiri <atahiri@e0r3p7.1337.ma>
2021-10-02 14:07:02 +0100

    starting

--- parsing/main.c
+++ parsing/main.c
@@ -6,16 +6,39 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 10:49:49 by atahiri           #+#    #+#             */
-/*   Updated: 2021/10/01 12:01:42 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/10/02 12:23:38 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "minishell.h"
 
-int		main(int argc, char **argv)
+int	collect_and_check_cmd_line(char **cmd_line)
 {
+	*cmd_line = readline("MINISHELL$ ");
+	if (*cmd_line == NULL)
+	{
+		write(2, "\033[MINISHELL$ exit\n", 19);
+		exit(0);
+	}
+	if (cmd_line[0][0] == '\0')
+	{
+		free(*cmd_line);
+		return (0);
+	}
+	return (1);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char		*cmd_line;
+
 	(void)argc;
 	(void)argv;
-	printf("parsing branch");
+	(void)envp;
+	while (1)
+	{
+		collect_and_check_cmd_line(&cmd_line);
+		printf("%s", cmd_line);
+	}
 	return (0);
 }
--- parsing/minishell.h
+++ parsing/minishell.h
@@ -6,7 +6,7 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 11:59:13 by atahiri           #+#    #+#             */
-/*   Updated: 2021/10/01 12:01:06 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/10/02 12:22:41 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,35 @@
 # include <stdlib.h>
 # include <string.h>
 
+/* strerror() and printf() function */
+# include <stdio.h>
+
+/* GNU Readline Library */
+# include <readline/readline.h>
+# include <readline/history.h>
+
+/* signal() and kill() system call functions */
+# include <signal.h>
+
+/* open() function */
+# include <fcntl.h>
+
+/* read() and write() and close() functions */
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+/* malloc() and free() exit() function */
+# include <stdlib.h>
+
+/* errno external variable */
+# include <sys/errno.h>
+
+# include <string.h>
+# include <sys/stat.h>
+
+
 # define COLORSTART "\e[38;5;226m"
-# define NEWLINE "\e[1m\e[38;5;82m$> \e[0m"
 # define COLOREND "\e[0m"
 # define STRERRORCOLOR "\e[38;5;196m"
 
