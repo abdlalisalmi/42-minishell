10224b41e7ca9305733e1f2f332a175798ca7045
Amine Tahiri <atahiri@student.1337.ma>
2021-11-19 17:58:20 +0100

    norm common

--- common.h
+++ common.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   common.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
+/*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 11:59:13 by atahiri           #+#    #+#             */
-/*   Updated: 2021/11/19 12:24:51 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/11/19 17:26:43 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,53 +22,45 @@
 # include <errno.h>
 # include <limits.h>
 
-
-typedef struct 	s_env 
+typedef struct s_env
 {
-	char 		*name;
-	char 		*value;
+	char		*name;
+	char		*value;
 }				t_env;
 
-typedef struct	s_redirections
+typedef struct s_redirections
 {
 	int			type;
 	char		*file;
 }				t_redirections;
 
-typedef struct 			s_commands {
-	char 				**args;
+typedef struct s_commands {
+	char				**args;
 	int					n_args;
-
 	t_redirections		*redirect;
-	int 				n_redirect;
-
+	int					n_redirect;
 	int					fd[2];
-}						t_commands;
+}				t_commands;
 
 typedef struct s_all {
 	int			n_env;
 	t_env		*env;
-
 	int			exit_code;
-
 	int			n_commands;
-	t_commands 	*commands;
-
-	int 		heredoc;
-	int 		is_child;
-
+	t_commands	*commands;
+	int			heredoc;
+	int			is_child;
 }				t_all;
 
 t_all	g_all;
 
-
 /*
 ** Manage EnvVar functions
 */
-void collect_env(char **env);
-char *get_env(char *name);
-void sort_env(void);
-int	set_env(char *name, char *value);
+void	collect_env(char **env);
+char	*get_env(char *name);
+void	sort_env(void);
+int		set_env(char *name, char *value);
 
 /*
 ** Execution endpoint
